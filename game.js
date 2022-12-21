98e11fe10cebb7d514e0e5eba91192159bf1af75
minow3 <minow3@gmail.com>
2022-12-21 09:49:04 +0000

    increment score, update score function

--- game.js
+++ game.js
@@ -3,6 +3,10 @@ const options = document.querySelectorAll(".options");
 const message = document.querySelector('.message');
 console.log(options);
 
+// Score count
+let pScore = 0;
+let cScore = 0;
+
 // Game function
 options.forEach((option) => {
   option.addEventListener("click", function () {
@@ -11,9 +15,17 @@ options.forEach((option) => {
         const cInput = cOptions[Math.floor(Math.random() * 3)];
         // Comper player vs computer inputs
         compareInputs(pInput, cInput);
+        // Update scores
+        updateScore();
   });
 });
+// Update score 
+function updateScore() {
+  document.getElementById('pScore').textContent = pScore;
+  document.getElementById('cScore').textContent = cScore;
+}
 
+// Comparing inputs
 function compareInputs(pInput, cInput) {
   const currentMatch = `${pInput} vs ${cInput}`;
 
@@ -27,24 +39,31 @@ function compareInputs(pInput, cInput) {
   if (pInput === "Rock") {
     if (cInput === "Scissors") {
       message.innerHTML = (`${currentMatch} = You Win`);
+      pScore++;
     } else {
       message.innerHTML = (`${currentMatch} = Computer Wins`);
+      cScore++;
     }
   }
   // Paper
   else if (pInput === "Paper") {
     if (cInput === "Rock") {
       message.innerHTML = (`${currentMatch} = You Win`);
+      pScore++;
     } else {
       message.innerHTML = (`${currentMatch} = Computer Wins`);
+      cScore++;
+      
     }
   }
   // Scissors
   else {
     if (cInput === "Paper") {
       message.innerHTML = (`${currentMatch} = You Win`);
+      pScore++;
     } else {
       message.innerHTML = (`${currentMatch} = Computer Wins`);
+      cScore++;
     }
   }
 }
