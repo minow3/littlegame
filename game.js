6f8999ffb10646782ae912777589efbd51e2ad63
minow3 <minow3@gmail.com>
2022-12-21 09:29:38 +0000

    added coments for beter code seperation

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
+// Selectors
 const options = document.querySelectorAll(".options");
 const message = document.querySelector('.message');
 console.log(options);
+
+// Game function
 options.forEach((option) => {
   option.addEventListener("click", function () {
     const pInput = this.textContent;
